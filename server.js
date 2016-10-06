2af9187b1028f103c8440fc49776a3e851997098
Ted Jarbo <ted.jarbo@yale.edu>
2016-10-06 20:29:47 +0000

    Add nickname wandering-deer

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ var app = express();
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
+app.get('/nickname', function(request, response){
+    response.send('wandering-deer');
+});
 
 app.listen(process.env.PORT || 4000);
